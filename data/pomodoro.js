8087ced91db86459b7e170ec24d5051cd35e0692
ycydro <ycydro@users.noreply.github.com>
2025-04-01 22:05:43 +0800

    feat: added state property to timer

--- data/pomodoro.js
+++ data/pomodoro.js
@@ -1,4 +1,5 @@
 export const DEFAULT_MINUTES = 25;
 export const DEFAULT_SECONDS = 0;
+export const DEFAULT_STATE = 'pomodoro';
 
-export const timer = { minutes: DEFAULT_MINUTES, seconds: DEFAULT_SECONDS }
+export const timer = { state: DEFAULT_STATE, minutes: DEFAULT_MINUTES, seconds: DEFAULT_SECONDS }
